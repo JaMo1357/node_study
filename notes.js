3783ae6031322f6bb5085e23a59a3f2d2aee4e96
unknown <jaroslavmohila@seznam.cz>
2019-02-20 23:15:30 +0100

    <initial>

---
+++ notes.js
@@ -0,0 +1,26 @@
+console.log("Starting notes.js ...");
+
+
+var addNote = (title, body) => {
+    console.log("New note", title, body);
+};
+
+var getAll = () => {
+    console.log("Getting all notes");
+};
+
+var readNote = (title) => {
+    console.log("Reading note ", title);
+};
+
+var removeNote = (title) => {
+    console.log("Removing note", title);
+};
+
+module.exports = { 
+    addNote
+    ,getAll 
+    ,readNote 
+    ,removeNote 
+};
+
